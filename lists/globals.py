38581aa7a24b1a9c6039a8a5c8d408770a458b00
livovachkov <livovachkov@gmail.com>
2021-08-22 21:59:17 +0300

    Added global file for mapping to the tests

---
+++ lists/globals.py
@@ -0,0 +1,3 @@
+#not really needed/self-made to store global function and variables(therefore must be included)
+
+command = "<title>To-Do lists</title><body>hello world</body>"
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from lists.globals import command
+
 # Create your views here.
 
-command = "<title>To-Do lists</title><body>hello world</body>"
+#command = "<html><title>To-Do lists</title><body>hello world</body></html>"
 
 def home_page(request):
     return HttpResponse(command)
