31e28541393b66b20de9fe4efd02663702f6fbeb
livovachkov <livovachkov@gmail.com>
2021-08-22 22:50:08 +0300

    Added templates and removed globals

--- lists/globals.py
+++
@@ -1,3 +0,0 @@
-#not really needed/self-made to store global function and variables(therefore must be included)
-
-command = "<title>To-Do lists</title><body>hello world</body>"
--- lists/views.py
+++ lists/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-from lists.globals import command
+#from lists.globals import command //the file was removed in change of refractoring
 
 # Create your views here.
 
 #command = "<html><title>To-Do lists</title><body>hello world</body></html>"
 
 def home_page(request):
-    return HttpResponse(command)
+    #return HttpResponse(command) #goes with command from lists.globals
+    return render(request, 'home.html')
