bae955bb67e78d34cdbdbde37232f66ed1e999e5
livovachkov <livovachkov@gmail.com>
2021-08-28 21:23:50 +0300

    Fixed and collected static

--- superlists/settings.py
+++ superlists/settings.py
@@ -29,11 +29,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+#    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
